8b1f07d05a7a6c44a3894200e1eaba9cf8740fa1
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-03-12 17:12:36 +0100

    fixed code for task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,19 @@
-import redisClient from "../utils/redis"
-import dbClient from "../utils/db";
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(_, res) {
     const isAlive = redisClient.isAlive();
     const isDb = dbClient.isAlive();
 
-    res.status(200).send({ "redis": isAlive, "db": isDb });
+    res.status(200).send({ redis: isAlive, db: isDb });
   }
 
   static getStats(_, res) {
     const users = dbClient.nbUsers();
     const files = dbClient.nbFiles();
 
-    res.status(200).send({ "users": users, "files": files });
+    res.status(200).send({ users, files });
   }
 }
 
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import routes from './routes/index';
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use('/', routes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on localhost port ${PORT}`);
+  console.log(`Server running on localhost port ${PORT}`);
 });
 
 export default app;
